39d6cb92568b04d5aa878ed591e307d9121b634a
dhieisonMS <jhasonx@gmail.com>
2022-10-13 00:27:01 -0300

    Add regex and new models

--- src/Model/Parking.cs
+++ src/Model/Parking.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace src.Model;
 
 public class Parking
 {
-    public int vagas;
+    public Parking(){}
 
+    public List<string> _veiculos = new List<string>();
+
+    public void GetVeiculos()
+    {
+        
+        if(_veiculos.Count == 0)
+        {
+            Console.WriteLine("Nenhum Veiculo cadastrado");
+        }
+        else
+        {
+            foreach (var countveiculos in _veiculos)
+            {  
+                Console.WriteLine("Veiculos Cadastrados:");
+                Console.WriteLine($"\t{countveiculos}");
+            }
+        }
+        
+    }
+
+    public bool ValidaPlaca(string placa)
+    {
+        Regex regex = new Regex(@"^[A-Za-z]{3}[0-9][0-9A-Z][0-9]{2}$");
+        
+        if(regex.IsMatch(placa))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void SetVeiculo()
+    {
+        Console.WriteLine("Informe sua placa");
+        string placa = Console.ReadLine();
+        bool result = ValidaPlaca(placa);
+
+        if (!result)
+        {
+            Console.WriteLine("Placa invalida digite novamente conforme o exemplo: AAA1234");
+        }
+        else
+        {
+            _veiculos.Add(placa);
+            Console.WriteLine($"Placa cadastrada{placa}");
+           
+            
+        }
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,40 @@
 ﻿using src.Model;
 
-Parking one = new Parking();
-one.vagas = 1;
+public class Program
+{
+    static void Main (string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Hello, welcome");
+            Console.WriteLine("Select a option:\n");
+            Console.WriteLine("\t 1 - Register Vehicle");
+            Console.WriteLine("\t 2 - List Vehicle");
+            Console.WriteLine("\t 3 - Remove Vehicle");
+            
+            Parking teste = new Parking();
 
-Console.WriteLine(one.vagas);
+            teste._veiculos.Add("teste");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    teste.SetVeiculo();
+                    
+                    break;
+                case "2":
+                    teste.GetVeiculos();
+                    
+                    break;
+                case "3":
+                    Console.WriteLine("3");
+                    break;
+                default:
+                    Console.WriteLine("opção invalida");
+                    break;
+            }   
+        }
+        
+        
+    }
+}
